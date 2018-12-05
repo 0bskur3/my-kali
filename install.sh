664243ad8cb68e8f7f08810f45c70c38ba81d15c
0bskur3 <0bskur3@protonmail.com>
2018-12-04 20:38:48 -0500

    Add gobuster and Seclists

--- install.sh
+++ install.sh
@@ -2,4 +2,17 @@
 
 # Setup Kali Linux
 
+# Install gobuster
+sudo apt-get install -y golang-go
+mkdir $HOME/.go
+mkdir $HOME/.go/bin
+cd $HOME/.go/
+git clone https://github.com/OJ/gobuster.git
+cd gobuster
+go get github.com/OJ/gobuster
+go build
+ln -s gobuster $HOME/.go/bin/gobuster
+
+# Install SecLists
+git clone https://github.com/danielmiessler/SecLists.git /usr/share/wordlists/SecLists
 
